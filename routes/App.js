5ff6df964391a989fe1c8df8ed4f43c6cf036963
samnoon <samnoonabrar@gmail.com>
2021-06-21 00:09:57 +0600

    added view engines

--- routes/App.js
+++ routes/App.js
@@ -5,12 +5,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(morgan("dev"));
+app.use( express.static( "public" ) );
 app.use("/student", require("./StudentRoutes"));
 app.use("/course", require("./CourseRoutes"));
 app.get("/", (req, res) => {
     res.render('./index');
 })
-app.get("/admin", (req, res) => {
+app.get("/public/Baiust.jpg", (req, res) => {
+    res.render('./index');
+})
+
+app.get("/admindashboard", (req, res) => {
     res.render('./admin/adminDashboard');
+})
+app.get("/studentdashboard", (req, res) => {
+    res.render('./student/studentDashboard');
+})
+app.get("/departmentdashboard", (req, res) => {
+    res.render('./department/departmentDashboard');
 })
 module.exports = app;
