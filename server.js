4e19b7a5020d8789758b57e50a172156acbfc400
samnoon <samnoonabrar@gmail.com>
2021-06-29 22:07:58 +0600

    updated gitignore

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://samnoon:admin123@server0.wdwol.mongodb.net/server0?retryWrites=true&w=majority";
+
 const app = express();
+require('dotenv').config();
+
 const api = require('./routes/App');
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const PORT = 3000;
+const PORT = process.env.PORT;
+const URI = process.env.URL;
 app.all("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -14,7 +17,7 @@ app.all("/*", function(req, res, next){
     next();
 });
 app.use(api);
-mongoose.connect("mongodb+srv://samnoon:admin123@server0.wdwol.mongodb.net/server0?retryWrites=true&w=majority",
+mongoose.connect(URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
