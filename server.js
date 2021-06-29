2ea4ec4f03828fb67230c0280469cd54536dd41d
samnoon <samnoonabrar@gmail.com>
2021-06-29 22:11:53 +0600

    updated gitignore

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
 require('dotenv').config();
-
 const api = require('./routes/App');
 app.set("view engine", "ejs");
 app.use(express.json());
